39399009b1459436805c2be2ce152b9565df15de
Hiroki.T <hirodora@me.com>
2019-05-17 18:22:23 +0900

    add: get task from redis

--- src/akapen.nim
+++ src/akapen.nim
@@ -1,5 +1,30 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+import redis
+import os
+import threadpool
+
+proc run(task: string) {.thread.} =
+    os.sleep(1000)
+    echo task
+
+proc main(): void =
+    let redis_client: redis.Redis = redis.open()  # redis client
+
+    # set pool size
+    setMaxPoolSize(256)
+    setMinPoolSize(256)
+
+    while true:
+        let task = redis_client.rPop("taskqueue")  # pop task (json)
+        
+        if task != redis.redisNil:
+            threadpool.spawn run(task)
+        else:
+            # if queue is empty, sleep for performance
+            os.sleep(1000)
+
+
 when isMainModule:
-  echo("Hello, World!")
+    main()
