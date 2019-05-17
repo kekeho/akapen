23f0913349a1cf0cfa4b226cb4a37293cce64276
Hiroki.T <hirodora@me.com>
2019-05-17 23:45:07 +0900

    fix:  securiy measures(shell injection) and get stderr

--- src/akapen.nim
+++ src/akapen.nim
@@ -6,6 +6,8 @@ import os
 import threadpool
 import osproc
 import json
+import strutils
+import streams
 
 
 proc run(task:string, redis_client:redis.Redis) {.thread.} =
@@ -18,12 +20,25 @@ proc run(task:string, redis_client:redis.Redis) {.thread.} =
 
     var
         output: string
-        err: int
+        err: string
         status: string
 
-    (output, err) = osproc.execCmdEx("echo \"" & input & "\" | " & "docker run akapen/" & lang & " \"" & code & "\"")
+    let p: osproc.Process = osproc.startProcess("docker",
+            args=["run", "-i", "akapen/$#" % [lang], code], 
+            options={poUsePath}
+        )
+    p.inputStream.write(input)
 
-    if err != 0:
+    if p.running:
+        while true:
+            if not p.running:
+                break
+    output = p.outputStream.readAll()
+    err = p.errorStream.readAll()
+
+    p.close()
+
+    if err.len != 0:
         status = "RE"
     # elif output != tasknode["assert"].getStr:
         # status = "WA"
@@ -32,6 +47,7 @@ proc run(task:string, redis_client:redis.Redis) {.thread.} =
     
     tasknode["status"] = %* status
     tasknode["output"] = %* output
+    tasknode["stderr"] = %* err
     let redis_result = redis_client.lPush("taskresults", tasknode.pretty)
 
 proc main(): void =
