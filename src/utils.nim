0885057290f3a85c366664dfd38e262e8ee0aed0
Hiroki.T <hirodora@me.com>
2019-05-21 20:53:14 +0900

    fix: exec_time

--- src/utils.nim
+++ src/utils.nim
@@ -6,6 +6,7 @@ import cpuinfo
 import random
 import os
 import times
+from math import pow
 
 type status* = enum
     CD = "Compiled"  # Waiting execute
@@ -30,6 +31,20 @@ proc rand_core(): int =
     return rand(0..cpuinfo.countProcessors()-1)
 
 
+proc interval_to_nanosec(interval: times.TimeInterval): int64 =
+    var nanosec: int64 = interval.nanoseconds
+    nanosec += interval.microseconds * 10.0.pow(3.0).int
+    nanosec += interval.milliseconds * 10.0.pow(6.0).int
+    nanosec += interval.seconds * 10.0.pow(9.0).int
+    nanosec += interval.minutes * 10.0.pow(9.0).int * 60
+    nanosec += interval.hours * 10.0.pow(9.0).int * 3600
+    nanosec += interval.days * 10.0.pow(9.0).int * 3600 * 24
+    nanosec += interval.weeks * 10.0.pow(9.0).int * 3600 * 24 * 7
+    nanosec += interval.months * 10.0.pow(9.0).int * 3600 * 24 * 7 * 31
+    nanosec += interval.years * 10.0.pow(9.0).int * 3600 * 24 * 7 * 31 * 12
+    return nanosec
+
+
 proc docker_run*(mode: docker_run_mode, arguments: seq[string], standard_input: string = "", memory : string = "", nanosec: int = -1): array[4, string] =
     ## Run docker container with arguments and stdin
     var args: seq[string] = @["run"]
@@ -43,16 +58,16 @@ proc docker_run*(mode: docker_run_mode, arguments: seq[string], standard_input: 
         args &= @["--cpu-period=100000", "--cpu-quota=5000"]  # CPU: 5%
 
     args &= arguments
-    let start_time: Time = getTime()
     let p: osproc.Process = osproc.startProcess("docker", args=args, options = {poUsePath})
-    
+    let start_time: times.TimeInterval = getTime().toTimeInterval
+
     # stdin
     if standard_input != "":
         p.inputStream.write(standard_input)
         p.inputStream.close()
     
     let exit_status: string = p.waitForExit().intToStr
-    let end_time: Time = getTime()
+    let end_time: times.TimeInterval = getTime().toTimeInterval
     
     # stdout, stderr
     let output = p.outputStream.readAll()
@@ -60,8 +75,8 @@ proc docker_run*(mode: docker_run_mode, arguments: seq[string], standard_input: 
 
     p.close()
     
-    let exec_time: int = (end_time - start_time).nanoseconds
-    return [output, err, exit_status, exec_time.intToStr]
+    let exec_time: int64 = (end_time - start_time).interval_to_nanosec
+    return [output, err, exit_status, $exec_time]
 
 
 proc get_status*(standard_output: string, standard_error: string, assertion: string, time: int, exec_time: int): status = 
